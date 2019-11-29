e4c3c278599120d19c05f622585011230b1553fc
LogosItAcademy <Tester99037@gmail.com>
2019-11-29 11:46:20 +0200

    first commit

---
+++ Java_Advanced_lesson-01/src/test/java/ua/lviv/lgs/RunAllTests.java
@@ -0,0 +1,13 @@
+
+package ua.lviv.lgs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TimeTest.class})
+
+public class RunAllTests {
+	
+}
